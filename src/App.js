7d4c37afe130bd2ee99eaa8bd006b67c749fc704
Alex Pritchard <alexbeet1234@gmail.com>
2022-01-31 16:07:44 +0800

    added lazy loading to components in the app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import { Fragment } from "react";
-import React from "react";
+import React, { Fragment, Suspense } from "react";
 
+import LoadingSpinner from "./UI/LoadingSpinner";
 import MainHeader from "./components/Layout/MainHeader";
 import Layout from "./components/Layout/Layout";
-import PostList from "./components/Post/PostList";
-import Post from "./components/Post/Post";
-import NotFound from "./components/Layout/NotFound";
+
+const PostList = React.lazy(() => import("./components/Post/PostList"));
+const Post = React.lazy(() => import("./components/Post/Post"));
+const NotFound = React.lazy(() => import("./components/Layout/NotFound"));
 
 function App() {
   return (
     <Fragment>
       <Layout>
-        <MainHeader />
-        <Routes>
-          <Route path="/" element={<PostList />} />
-          <Route path="/post/:postId" element={<Post />} />
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <MainHeader />
+          <Routes>
+            <Route path="/" element={<PostList />} />
+            <Route path="/post/:postId" element={<Post />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </Fragment>
   );
